7c0af5064e5f7e2dbdcaf60910eb67d999199e96
Ariel Bernal <ariel.j.bernal@intel.com>
2017-07-28 20:28:25 -0700

    SA

--- delta.h
+++ delta.h
@@ -10,11 +10,11 @@ static float toRad(float deg) {
 
 struct DeltaGeometry {
   // Geometry description ------------------------------------------------------------------------
-  float RodRadius = 174.50;       // from carriage joint to effector joint  
+  float RodRadius = 176.50;       // from carriage joint to effector joint  
   float Alpha = 90;               // angle from x-axis to A tower
   float Beta = 210;               // angle from x-axis to B tower
   float Gamma = 330;              // angle from x-axis to C tower 
-  float RodLength = 360.40;       // rod length from joint to joint
+  float RodLength = 362;          // rod length from joint to joint
 
   float RadiusCorrectionA = 0;    // Delta radius correction A tower
   float RadiusCorrectionB = 0;    // Delta radius correction B tower
@@ -25,6 +25,10 @@ struct DeltaGeometry {
   float TowerAOffset = 0;         // Tower A offset
   float TowerBOffset = 0;         // Tower B offset
   float TowerCOffset = 0;         // Tower C offset
+  void print() {
+    printf("RR = %f RL = %f A(%f, %f, %f) O(%f, %f, %f) RC(%f, %f, %f)\n", RodRadius, RodLength, Alpha, Beta, Gamma,
+     TowerAOffset, TowerBOffset, TowerCOffset, RadiusCorrectionA, RadiusCorrectionB, RadiusCorrectionC);
+  }
 };
 
 struct DeltaPrinter {
@@ -164,22 +168,29 @@ struct DeltaPrinter {
   }
 
   DeltaGeometry geoNeighbour(DeltaGeometry& geo) {
-    int geoField = rand() % 4;
-    int change = rand() % 2;
+    int geoField;
+    int change;
     DeltaGeometry t = geo;
-    switch(geoField) {
-      case 0 : t.TowerAOffset += (change ? 0.1 : -0.1); break;
-      case 1 : t.TowerBOffset += (change ? 0.1 : -0.1); break;
-      case 2 : t.TowerCOffset += (change ? 0.1 : -0.1); break;
-      // case 3 : t.RodRadius *= change ? 0.9 : 1.1; break;
-      // case 4 : t.Alpha *= change ? 0.9 : 1.1; break;
-      // case 5 : t.Beta *= change ? 0.9 : 1.1; break;
-      // case 6 : t.Gamma *= change ? 0.9 : 1.1; break;
-      // case 7 : t.RadiusCorrectionA *= change ? 0.9 : 1.1; break;
-      // case 8 : t.RadiusCorrectionB *= change ? 0.9 : 1.1; break;
-      // case 9 : t.RadiusCorrectionC *= change ? 0.9 : 1.1; break;
+    float dTOffset = 0.01;
+    for (int i = 0; i < 1; ++i) {
+      geoField = rand() % 3;
+      change = rand() % 2;
+      switch(geoField) {
+        case 0 : t.TowerAOffset += (change ? dTOffset : -dTOffset); break;
+        case 1 : t.TowerBOffset += (change ? dTOffset : -dTOffset); break;
+        case 2 : t.TowerCOffset += (change ? dTOffset : -dTOffset); break;
+        case 3 : t.RodRadius += (change ? 0.01 : -0.01); break;
+        case 4 : t.RodLength += (change ? 0.01 : -0.01); break;
 
+        // case 4 : t.Alpha += change ? 0.01 : -0.01; break;
+        // case 5 : t.Beta += change ? 0.01 : -0.01; break;
+        // case 6 : t.Gamma += change ? 0.01 : -0.01; break;
+        // case 7 : t.RadiusCorrectionA += change ? 0.01 : -0.01; break;
+        // case 8 : t.RadiusCorrectionB += change ? 0.01 : -0.01; break;
+        // case 9 : t.RadiusCorrectionC += change ? 0.01 : -0.01; break;
+      }
     }
+
     return t;
   }
 
@@ -200,33 +211,41 @@ struct DeltaPrinter {
     v[6] = float4(-r * cos(toRad(Gamma)), -r * sin(toRad(Gamma)), 0);
     float z[N] = { 0.3, -0.1, 0.2, -1.2, -0.5, 1.3, 0.3 };
 
-    DeltaGeometry Geo = FKGeo;
+    //FKGeo.TowerAOffset = 2.5; FKGeo.TowerBOffset = 2.1; FKGeo.TowerCOffset = 3.5;
 
     float T = 1E10;
     float TMin = 0.1;
     float TAlpha = 0.99;
-    float old_cost = cost(IKGeo, Geo, v, z, N);
-    float d = 1e-5;
+    float old_cost = cost(IKGeo, FKGeo, v, z, N);
+    float best_cost = old_cost;
+    float initial_cost = old_cost;
+    float k = 1e-8; // (1.6*(pow(10,-23)));
     printf("Cost = %f\n", old_cost);
     int NIter = 100;
     int itotal = 0;
+    DeltaGeometry bestGeo = FKGeo;
     while(T > TMin) {
       int i = 0;
       while (i < NIter) {
-        DeltaGeometry NewGeo = geoNeighbour(Geo);
-        float new_cost = cost(IKGeo, Geo, v, z, N);
+        DeltaGeometry NewGeo = geoNeighbour(FKGeo);
+        float new_cost = cost(IKGeo, NewGeo, v, z, N);
         if (new_cost == new_cost) { // check if not nan
-          if(new_cost < old_cost || exp(-(new_cost - old_cost) / (d * T)) > randf()) {
-            Geo = NewGeo;
+          if(new_cost < old_cost || exp(-(new_cost - old_cost) / (k * T)) > randf()) {
+            FKGeo = NewGeo;
             old_cost = new_cost;
-            printf("%d Cost = %f T = %f  -  ", itotal, old_cost, T);
-            printf("  OA=%f OB=%f OC=%f\n", Geo.TowerAOffset, Geo.TowerBOffset, Geo.TowerCOffset);
+      printf("%d - Initial Cost = %f - Best Cost = %f - Cost = %f T = %f  -  ", itotal, initial_cost, best_cost, new_cost, T);
+            FKGeo.print();              
+      
+            if (new_cost < best_cost) {
+              best_cost = new_cost;
+              bestGeo = NewGeo;
+            }
           }
           i++;
           itotal++;
         }
       }
-      printf("--------------------------------- T = %f\n", T);
+      //printf("--------------------------------- T = %f\n", T);
       T *= TAlpha;
     }
   }
